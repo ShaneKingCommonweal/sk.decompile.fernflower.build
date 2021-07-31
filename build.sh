78745dabd101845ae75909fb7fbc7b3f0cf3fe11
ShaneKing <github@shaneking.org>
2021-07-31 18:19:59 +0800

    Update and rename offline.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+[[ -d fernflower ]] && rm -rf fernflower
+git clone "https://github.com/fesh0r/fernflower.git"
+cd fernflower
+git submodule init && git submodule update
+gradle clean build
+ls -alh .
+ls -alh ./build
+cd ../
+
+[[ -d sk.decompile.fernflower.build ]] && rm -rf sk.decompile.fernflower.build
+git clone "https://github.com/ShaneKingCommonweal/sk.decompile.fernflower.build.git"
+rm -rf sk.decompile.fernflower.build/build
+cp -r fernflower/build sk.decompile.fernflower.build
+
+cd sk.decompile.fernflower.build
+git pull
+#can not add target...
+#git add -A
+git add -f .
+git add -u
+git commit -m "sync at $(date +'%Y-%m-%d %H:%M')"
+git push -f "https://ShaneKing:${GH_TOKEN}@github.com/ShaneKingCommonweal/sk.decompile.fernflower.build.git" main:main
